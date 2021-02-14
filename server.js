e2b8a8985e9468346af5d491b143e8d0c40ff925
Abhishek Singh <abhishek17513@gmail.com>
2021-02-14 20:34:08 +0530

    changes

--- server.js
+++ server.js
@@ -21,15 +21,12 @@ app.use(express.json());
 // Use Routes
 app.use('/api/items', items);
 
-// serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // set static folder
-  app.use(express.static('client/build'));
+// set static folder
+app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
