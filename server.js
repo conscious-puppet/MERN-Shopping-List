a6e7767058e86fac249f7989b0abb93eae710df7
Abhishek Singh <abhishek17513@gmail.com>
2021-02-17 13:40:01 +0530

    set static foldeer

--- server.js
+++ server.js
@@ -21,12 +21,12 @@ app.use('/api/items', require('./routes/api/items'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-// // set static folder
-// app.use(express.static('client/build'));
+// set static folder
+app.use(express.static('client/build'));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
